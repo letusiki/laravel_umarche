b9b7178a1627c840a3c08ed6cfbd3800370aefc6
letusiki <iki.shunsuke@letus.co.jp>
2022-06-02 10:06:20 +0900

    サービスプロバイダのテスト

--- app/Http/Controllers/LifeCycleTestController.php
+++ app/Http/Controllers/LifeCycleTestController.php
@@ -7,6 +7,15 @@ use Illuminate\Http\Request;
 class LifeCycleTestController extends Controller
 {
     //
+    public function showServiceProviderTest()
+    {
+      $encrypt=app()->make('encrypter');
+      $password=$encrypt->encrypt('password');
+
+      $sample=app()->make('serviceProviderTest');
+
+      dd($sample,$password, $encrypt->decrypt($password));
+    }
     public function showServiceContainerTest()
     {
         app()->bind('lifeCycleTest',function(){
@@ -22,6 +31,7 @@ class LifeCycleTestController extends Controller
         app()->bind('sample',Sample::class);
         $sample=app()->make('sample');
         $sample->run();
+
         dd($test,app());
     }
 }
